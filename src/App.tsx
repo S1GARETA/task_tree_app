7d5ae0a9be2ae33011d768da5ff2ac4c8b6c50fa
S1GARETA <rabota.dsv@mail.ru>
2025-03-13 22:21:26 +0500

    change style

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = observer(() => {
     return (
         <div className="flex justify-center items-center">
             <div className="bg-sky-400 w-2xl h-auto mt-8 m-auto p-5 rounded-2xl shadow-2xl">
-                <h1 className="text-4xl text-center mb-2">Дерево задач</h1>
+                <h1 className="text-3xl text-center mb-4">Дерево задач</h1>
                 <button 
                     onClick={() => TaskStore.addTask('New Task')} 
                     className="w-40 h-12 bg-sky-800 hover:bg-sky-900 text-white rounded-xl"
